a09a051fafd58be724b82c1d7180d7a7092d66ec
Unravel <1621690242@qq.com>
2022-10-12 00:36:43 +0800

    update games

--- env.py
+++ env.py
@@ -1,13 +1,10 @@
-import math
 import sys
-from contextlib import closing
+from copy import copy
 from enum import Enum
-from io import StringIO
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
-from gym import utils
 import numpy as np
-from gym.utils import seeding
+from gym import utils
 
 
 class TaxiEnv:
@@ -26,110 +23,90 @@ class TaxiEnv:
             self.pass_idxes: List[int] = []
             self.dst_idxes: List[int] = []
 
-    def __init__(self, map_str: List[str], locs: List[Tuple[int, int]],
-                 num_pass: int, max_steps=200):
+    def __init__(self, map_str: List[str],
+                 locs_map: Dict[str, Tuple[int, int]],
+                 num_pass: int,
+                 max_steps=200
+                 ):
         self.desc = np.asarray(map_str, dtype="c")
-        self.locs = locs
+        self.locs = []
+        for w, (r, c) in locs_map.items():
+            self.locs.append((r, c))
+            self.desc[r + 1, c * 2 + 1] = w[0]
+        locs = self.locs
         self.num_pass = num_pass
         self.max_steps = max_steps
 
         self.num_rows = num_rows = len(map_str) - 2  # 去除最上最下两行
-        self.num_cols = num_cols = (len(map_str[0]) - 1) / 2
+        self.num_cols = num_cols = int((len(map_str[0]) - 1) / 2)
         self.num_locs = num_locs = len(locs)
-
         self.action_space = self.EnumAction.__len__()
-
         self.observation_space = num_rows * num_cols * (num_locs + 1) ** num_pass * num_locs ** num_pass
-        self.np_random, seed = seeding.np_random()
 
         self._elapsed_steps = 0
         self._total_reward = 0
-        self._current_state = self._init_state()
-        self._left_pass = num_pass
+        self._origin_state = self._init_state()
+        self._current_state = copy(self._origin_state)
         self._last_action = None
 
     def _init_state(self) -> State:
-        return self.State()
+        loop = True
+        while loop:
+            i = np.random.randint(self.observation_space)
+            state = self.decode(i)
+            taxi_loc = (state.taxi_row, state.taxi_col)
+            loop = False
+            if taxi_loc in self.locs:
+                loop = True
+                continue
+            for i in range(self.num_pass):
+                if state.pass_idxes[i] == state.dst_idxes[i] \
+                        or self._in_taxi(state.pass_idxes[i]):
+                    loop = True
+                    break
+            if not loop:
+                return state
+
+    def reset(self):
+        self._elapsed_steps = 0
+        self._total_reward = 0
+        self._origin_state = self._init_state()
+        self._current_state = copy(self._origin_state)
+        self._last_action = None
 
     def encode(self, state: State):
         opt = state.taxi_row
-        opt *= self.num_rows
-        opt += state.taxi_col
         opt *= self.num_cols
+        opt += state.taxi_col
         for i in range(self.num_pass):
-            opt += state.pass_idxes[i]
             opt *= (self.num_locs + 1)
-        for i in range(self.num_pass - 1):
-            opt += state.dst_idxes[i]
+            opt += state.pass_idxes[i]
+        for i in range(self.num_pass):
             opt *= self.num_locs
-        opt += state.dst_idxes[self.num_pass - 1]
+            opt += state.dst_idxes[i]
         return opt
-
-    # def encode(self, taxi_row: int, taxi_col: int, pass_locs: List[int], dst_locs: List[int]):
-    #     opt = taxi_row
-    #     opt *= self.num_rows
-    #     opt += taxi_col
-    #     opt *= self.num_cols
-    #     for i in range(self.num_pass):
-    #         opt += pass_locs[i]
-    #         opt *= (self.num_locs + 1)
-    #     for i in range(self.num_pass - 1):
-    #         opt += dst_locs[i]
-    #         opt *= self.num_locs
-    #     opt += dst_locs[self.num_pass - 1]
-    #     return opt
 
     def decode(self, ipt: int):
         assert 0 <= ipt < self.observation_space
         state = self.State()
         dst_locs = []
-        for i in range(self.num_pass - 1):
-            dst_locs.append(ipt % self.num_locs)
+        for i in range(self.num_pass):
+            dst_locs.append(int(ipt % self.num_locs))
             ipt = ipt // self.num_locs
-        dst_locs.append(ipt % (self.num_locs + 1))
-        ipt //= self.num_locs + 1
         dst_locs.reverse()
         state.dst_idxes = dst_locs
 
         pass_locs = []
-        for i in range(self.num_pass - 1):
-            pass_locs.append(ipt % (self.num_locs + 1))
+        for i in range(self.num_pass):
+            pass_locs.append(int(ipt % (self.num_locs + 1)))
             ipt //= (self.num_locs + 1)
-        pass_locs.append(ipt % self.num_cols)
-        ipt //= self.num_cols
         pass_locs.reverse()
         state.pass_idxes = pass_locs
 
-        state.taxi_col = ipt % self.num_rows
-        ipt //= self.num_rows
-        state.taxi_row = ipt
+        state.taxi_col = int(ipt % self.num_cols)
+        ipt //= self.num_cols
+        state.taxi_row = int(ipt)
         return state
-
-    # def decode(self, ipt):
-    #     assert 0 <= ipt < self.observation_space
-    #     out = []
-    #     dst_locs = []
-    #     for i in range(self.num_pass - 1):
-    #         dst_locs.append(ipt % self.num_locs)
-    #         ipt = ipt // self.num_locs
-    #     dst_locs.append(ipt % (self.num_locs + 1))
-    #     ipt //= self.num_locs + 1
-    #     dst_locs.reverse()
-    #     out.append(dst_locs)
-    #
-    #     pass_locs = []
-    #     for i in range(self.num_pass - 1):
-    #         pass_locs.append(ipt % (self.num_locs + 1))
-    #         ipt //= (self.num_locs + 1)
-    #     pass_locs.append(ipt % self.num_cols)
-    #     ipt //= self.num_cols
-    #     pass_locs.reverse()
-    #     out.append(pass_locs)
-    #
-    #     out.append(ipt % self.num_rows)
-    #     ipt //= self.num_rows
-    #     out.append(ipt)
-    #     return reversed(out)
 
     def _in_taxi(self, pass_idx: int):
         return pass_idx == self.num_locs
@@ -137,8 +114,18 @@ class TaxiEnv:
     def _get_in_taxi(self, pass_idxes: List[int], i: int):
         pass_idxes[i] = self.num_pass
 
-    def _get_off_taxi(self, pass_idxes: List[int], i: int, new_loc_idx: int):
+    @staticmethod
+    def _get_off_taxi(pass_idxes: List[int], i: int, new_loc_idx: int):
         pass_idxes[i] = new_loc_idx
+
+    def _num_delivered(self, state: State) -> int:
+        """送达目的地的乘客数"""
+        delivered = 0
+        for i in range(self.num_pass):
+            pass_idx = state.pass_idxes[i]
+            if self._in_taxi(pass_idx):
+                delivered += 1
+        return delivered
 
     def _step(self, action: int) -> Tuple[int, int, bool, dict]:
         act = self.EnumAction(action)
@@ -174,10 +161,10 @@ class TaxiEnv:
                     self._get_off_taxi(state.pass_idxes, i, dst_idx)
                     reward += 20
                     match = True
-                    self._left_pass -= 1
-                    if self._left_pass == 0:
+                    if self._num_delivered(state) == self.num_pass:
                         done = True
                     break
+
             if not match and (taxi_loc in self.locs):
                 in_taxi = []
                 for i in range(self.num_pass):
@@ -204,7 +191,17 @@ class TaxiEnv:
             done = True
         return state, reward, done, info
 
-    def render(self):
+    def print_state(self):
+        print('current state:')
+        for name, value in vars(self._current_state).items():
+            print('%s=%s' % (name, value))
+
+    def print(self):
+        print("Taxi Env:")
+        for name, value in vars(self).items():
+            print('%s=%s' % (name, value))
+
+    def render(self, has_text=True):
         outfile = sys.stdout
 
         out = self.desc.copy().tolist()
@@ -236,18 +233,26 @@ class TaxiEnv:
         for dst_idx in state.dst_idxes:
             di, dj = self.locs[dst_idx]
             out[1 + di][2 * dj + 1] = utils.colorize(out[1 + di][2 * dj + 1], "magenta")
-            outfile.write("\n".join(["".join(row) for row in out]) + "\n")
+
+        outfile.write("\n".join(["".join(row) for row in out]) + "\n")
         if self._last_action is not None:
-            outfile.write("  Last Action: {}".format(
-                self.EnumAction(self._last_action).name
-            )
-            )
+            outfile.write(f"Last action: {self.EnumAction(self._last_action).name}")
         outfile.write("\n")
 
-        if has_pass:
-            in_taxi = []
+        if has_text:
+            outfile.write('Delivery list:\n')
             for i in range(self.num_pass):
-                if self._in_taxi(state.pass_idxes[i]):
-                    in_taxi.append(i)
-            outfile.write("  Pass in taxi: {}".format(in_taxi))
-        outfile.write("\n")
+                origin_idx = self._origin_state.pass_idxes[i]
+                pass_idx = state.pass_idxes[i]
+                dst_idx = state.dst_idxes[i]
+                outfile.write(f'  Passenger {i}:  [{origin_idx}] {self.locs[origin_idx]}')
+                outfile.write(f' -> [{dst_idx}] {self.locs[dst_idx]}  ')
+                if self._in_taxi(pass_idx):
+                    outfile.write('IN TAXI')
+                elif pass_idx == dst_idx:
+                    outfile.write('FINISH')
+                else:
+                    outfile.write('WAITING')
+                outfile.write('\n')
+            outfile.write("\n")
+            outfile.write(f'TOTAL REWARD: {format(self._total_reward)}\n')
